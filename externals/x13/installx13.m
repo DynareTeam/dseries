dc54c3fd044698860eaeff275ed7fc5d05ae0a3b
Houtan Bastani <houtan@dynare.org>
2018-02-14 12:47:32 +0100

    add semicolon to suppress mkdir output on Octave

--- externals/x13/installx13.m
+++ externals/x13/installx13.m
@@ -2,7 +2,7 @@ function installx13()
 
 % Installs CENSUS X13 binaries (Windows and Linux).
 
-% Copyright (C) 2017 Dynare Team
+% Copyright (C) 2017-2018 Dynare Team
 %
 % This code is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -26,13 +26,13 @@ if ~exist('x13.zip','file')
     end
     unzip('x13.zip');
     if ismac()
-        mkdir osx
+        mkdir osx;
         movefile('binaries/osx/*','./osx');
     elseif isunix()
-        mkdir linux
+        mkdir linux;
         movefile('binaries/linux/*','./linux');
     elseif ispc()
-        mkdir windows
+        mkdir windows;
         movefile('binaries/windows/*','./windows');
     else
         error('X13 is not available for the current platform!')
